66dcacb3b7759f8497066d377178091c1fdca845
Aman Kedia <42097829+Aman-Kedia@users.noreply.github.com>
2023-09-13 12:28:00 +0530

    Update process-image.js

--- pages/api/process-image.js
+++ pages/api/process-image.js
@@ -1,12 +1,12 @@
-import formidable from 'formidable'
-import sharp from 'sharp'
-import fs from 'fs'
-import path from 'path'
-import { fileTypeFromBuffer } from 'file-type'
-import { NFTStorage, File } from 'nft.storage'
+import formidable from 'formidable';
+import sharp from 'sharp';
+// import fs from 'fs';
+// import path from 'path';
+import { fileTypeFromBuffer } from 'file-type';
+// import { NFTStorage, File } from 'nft.storage';
 
-const API_KEY = process.env.NEXT_PUBLIC_IPFS_API_KEY
-const client = new NFTStorage({ token: API_KEY })
+const API_KEY = process.env.NEXT_PUBLIC_IPFS_API_KEY;
+const client = new NFTStorage({ token: API_KEY });
 
 export const config = {
   api: {
@@ -19,29 +19,32 @@ export default async function handler(req, res) {
     return res.status(405).end(); // Method Not Allowed
   }
 
-  const form = formidable()
+  const form = formidable();
   form.parse(req, async (err, fields, files) => {
     try {
       if (err) {
-        return res.status(500).json({ error: 'Image processing failed.' })
+        return res.status(500).json({ error: 'Image processing failed.' });
       }
       if (!files.image) {
-        return res.status(400).json({ error: 'No image file provided.' })
+        return res.status(400).json({ error: 'No image file provided.' });
       }
-      const imgBuffer = fs.readFileSync(files.image[0].filepath)
-      const fileType = await fileTypeFromBuffer(imgBuffer)
+      const imgBuffer = fs.readFileSync(files.image[0].filepath);
+      const fileType = await fileTypeFromBuffer(imgBuffer);
       if (!fileType || !fileType.mime.startsWith('image/')) {
-        return res.status(400).json({ error: 'Inavlid file type.' })
+        return res.status(400).json({ error: 'Inavlid file type.' });
       }
-      const pngImageBuffer = await sharp(imgBuffer).toFormat('png', { quality: 30 }).toFile('nft.png')
-    
+      const pngImageBuffer = await sharp(imgBuffer)
+        .resize(500, 500)
+        .toFormat('png', { quality: 100 })
+        .toFile('nft.png')
+
       // const file = new File([pngImageBuffer], 'nft.png', { type: 'image/png' })
       // const cid = await client.storeBlob(file)
 
-      res.status(200).json({ message: 'Image processed successfully.' })
-    } catch(error) {
-      console.log(error)
-      return res.status(500).json({ error: 'Image processing failed.' })
+      res.status(200).json({ message: 'Image processed successfully.' });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Image processing failed.' });
     }
   })
 }
