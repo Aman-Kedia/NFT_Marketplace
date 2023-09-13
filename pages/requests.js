4d4bbb8b985b04f40c12e78b1f9fd244bd0e7390
Aman Kedia <42097829+Aman-Kedia@users.noreply.github.com>
2023-09-13 12:32:19 +0530

    Update requests.js

--- pages/requests.js
+++ pages/requests.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import Web3Modal from "web3modal"
 
+import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json"
 const nftMarketplaceAddress = process.env.NEXT_PUBLIC_NFT_MARKETPLACE_ADDRESS
 
 export default function Requests() {
 
-    const [requests, setRequests] = useState([])
-    const [isOwner, setIsOwner] = useState(false)
+    const [nfts, setNfts] = useState([])
+    const [isOwner, setIsOwner] = useState(true)
 
     useEffect(() => {
         loadRequests()
@@ -17,29 +20,33 @@ export default function Requests() {
         const provider = new ethers.providers.Web3Provider(connection)
         const signer = provider.getSigner()
         const contract = new ethers.Contract(nftMarketplaceAddress, NFTMarketplace.abi, signer)
-        //check id platform owner of not
+        const userAddress = await signer.getAddress()
+        const contractOwnerAddress = await contract.i_owner()
+        if (userAddress != contractOwnerAddress) {
+            setIsOwner(false)
+            return null
+        };
         return contract
     }
 
     async function loadRequests() {
         try {
-            const contract = getContract()
+            const contract = await getContract()
             const data = await contract.fetchTokensForApproval()
             const tokens = await Promise.all(data.map(async i => {
                 const tokenURI = await contract.tokenURI(i.tokenId)
                 const metadata = await axios.get(tokenURI)
-                const price = ethers.utils.formatUnits(i.price.toString(), 'ether')
                 const token = {
-                    price,
+                    price: i.price.toString(),
                     tokenId: i.tokenId.toNumber(),
-                    name: metadata.name,
-                    desc: metadata.description,
-                    image: metadata.image
+                    name: metadata.data.name,
+                    desc: metadata.data.description,
+                    image: metadata.data.image
                 }
                 return token
             }
             ))
-            setRequests(tokens)
+            setNfts(tokens)
         }
         catch (error) {
             console.log(error)
@@ -48,7 +55,7 @@ export default function Requests() {
 
     async function updateTokenRequests(tokenId, statusCode) {
         try {
-            const contract = getContract()
+            const contract = await getContract()
             const transaction = await contract.updateTokenStatus(tokenId, statusCode)
             await transaction.wait()
         }
@@ -58,8 +65,18 @@ export default function Requests() {
     }
 
     return (
-        <>
-            Requests
-        </>
+        <div className="dark:bg-slate-800 flex p-4 min-h-screen">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                {/* display proper message if not owner or no nft available */}
+                {Boolean(!isOwner) && Boolean(!nfts.length) && (
+                    <p className="text-white text-2xl">No Nfts</p>
+                )}
+                {Boolean(isOwner) && Boolean(nfts.length) && (
+                    nfts.map((nft, i) => (
+                        <NftCard key={i} nft={nft} updateTokenRequests={updateTokenRequests} pageName='requests' />
+                    ))
+                )}
+            </div>
+        </div >
     )
 }
